a2e61e5b5b9a5d553ab48343eb5b3595473a5feb
Sean李翔 <czlixiang@gmail.com>
2024-07-19 13:53:28 +0800

    init upload

---
+++ AddIn.cs
@@ -0,0 +1,408 @@
+﻿using Siemens.Engineering;
+using Siemens.Engineering.AddIn.Menu;
+using Siemens.Engineering.HW;
+using Siemens.Engineering.SW.Blocks;
+using System;
+using System.Collections.Generic;
+#if DEBUG
+            using System.Diagnostics;
+#endif
+using System.Globalization;
+using System.Linq;
+using System.IO;
+using Siemens.Engineering.Online;
+using System.Windows.Forms;
+using Siemens.Engineering.HW.Features;
+using Siemens.Engineering.SW;
+
+namespace TIA_Add_In_Intf2WCS
+{
+    public class AddIn : ContextMenuAddIn
+    {
+        #region Definition
+
+        /// <summary>
+        /// 博图实例
+        /// </summary>
+        private readonly TiaPortal _tiaPortal;
+
+        /// <summary>
+        /// Base class for projects
+        /// can be used in multi-user environment
+        /// </summary>
+        private ProjectBase _projectBase;
+
+        // /// <summary>
+        // /// Path of the project file
+        // /// </summary>
+        // private string _projectPath;
+
+        /// <summary>
+        /// Path of the project directory
+        /// </summary>
+        private string _projectDir;
+
+        /// <summary>
+        /// 导出文件夹路径
+        /// </summary>
+        private string _exportFileDir;
+
+        // /// <summary>
+        // /// 导出文件夹信息
+        // /// </summary>
+        // private DirectoryInfo _exportDirInfo;
+
+        // /// <summary>
+        // /// 获取PLC目标
+        // /// </summary>
+        // private PlcSoftware _plcSoftware;
+
+        /// <summary>
+        /// 获取实例的DB名称
+        /// </summary>
+        private string _globalDbName;
+
+        /// <summary>
+        /// 获取DB的编号
+        /// </summary>
+        private string _number;
+
+        /// <summary>
+        /// 获取程序语言类型
+        /// </summary>
+        private string _programmingLanguage;
+
+        /// <summary>
+        /// 获取InstanceDB Xml文件路径
+        /// </summary>
+        private string _iDbXmlFilePath;
+
+        /// <summary>
+        /// 写入数据流
+        /// </summary>
+        private StreamWriter _streamWriter;
+
+        /// <summary>
+        /// csv文件保存路径
+        /// </summary>
+        private string _csvFilePath;
+
+        #endregion
+
+        public AddIn(TiaPortal tiaPortal) : base("WCS工具")
+        {
+            _tiaPortal = tiaPortal;
+        }
+
+        protected override void BuildContextMenuItems(ContextMenuAddInRoot addInRootSubmenu)
+        {
+            addInRootSubmenu.Items.AddActionItem<GlobalDB>("生成数据", Generate_OnClick);
+            addInRootSubmenu.Items.AddActionItem<IEngineeringObject>("如需使用，请选中全局数据块", 
+                menuSelectionProvider => { }, GenerateStatus);
+        }
+
+        private void Generate_OnClick(MenuSelectionProvider<GlobalDB> menuSelectionProvider)
+        {
+#if DEBUG
+            Debugger.Launch();
+#endif
+            //获取项目数据
+            GetProjectData();
+            //确定PLC的在线状态
+            if (!IsOffline())
+            {
+                throw new Exception(string.Format(CultureInfo.InvariantCulture,
+                            "PLC 在线状态！"));
+            }
+
+            try
+            {
+                // 打开窗口获取.csv文件保存位置
+                SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    Filter = "逗号分隔值|*.csv",
+                    Title = "请选择保存文件的路径"
+                };
+
+                if (saveFileDialog.ShowDialog(new Form()
+                { TopMost = true, WindowState = FormWindowState.Maximized }) == DialogResult.OK)
+                {
+                    // 独占窗口
+                    using (ExclusiveAccess exclusiveAccess = _tiaPortal.ExclusiveAccess("导出中……"))
+                    {
+                        //定义csv数据流
+                        _csvFilePath = saveFileDialog.FileName;
+                        _streamWriter = new StreamWriter(_csvFilePath);
+
+                        //写入标题行
+                        _streamWriter.WriteLine("\"输送机编号\",\"变量名\",\"类型\",\"DB\",\"开始地址\",\"DB名称\"");
+
+                        foreach (GlobalDB globalDb in menuSelectionProvider.GetSelection())
+                        {
+                            if (exclusiveAccess.IsCancellationRequested)
+                            {
+                                break;
+                            }
+
+                            // 获取 GlobalDB 名称
+                            _globalDbName = globalDb.Name;
+                            // 获取Number
+                            _number = globalDb.Number.ToString();
+                            // 获取ProgrammingLanguage
+                            _programmingLanguage = globalDb.ProgrammingLanguage.ToString();
+
+                            //创建导出文件夹
+                            _exportFileDir = Path.Combine(_projectDir, "WCS");
+                            if (!Directory.Exists(_exportFileDir))
+                            {
+                                Directory.CreateDirectory(_exportFileDir);
+                            }
+                            if (Directory.Exists(_exportFileDir))
+                            {
+                                // 导出DB块
+                                _iDbXmlFilePath = Path.Combine(_exportFileDir, StringHandle(_globalDbName) + ".xml");
+                                exclusiveAccess.Text = "导出中-> " + Export(globalDb, _iDbXmlFilePath);
+                            }
+
+                            XmlReader xmlReader = new XmlReader
+                            {
+                                DbXmlFilePath = _iDbXmlFilePath,
+                                ProgrammingLanguage = _programmingLanguage,
+                                Number = _number,
+                                InstanceName = _globalDbName,
+                                StreamWriter = _streamWriter
+                            };
+                            xmlReader.Run();
+                        }
+                        _streamWriter.Close();
+
+                        //导出完成
+                        MessageBox.Show($"目标文件:{saveFileDialog.FileName}", "导出完成", 
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "异常", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //删除csv文件
+                if (File.Exists(_csvFilePath))
+                {
+                    File.Delete(_csvFilePath);
+                }
+                throw;
+            }
+            finally
+            {
+                // 删除导出的文件夹
+                DeleteDirectoryAndContents(_exportFileDir);
+            }
+        }
+        
+        /// <summary>
+        /// 导出PLC数据
+        /// </summary>
+        /// <param name="exportItem"></param>
+        /// <param name="exportPath"></param>
+        /// <returns></returns>
+        private string Export(IEngineeringObject exportItem, string exportPath)
+        {
+            const ExportOptions exportOption = ExportOptions.WithDefaults | ExportOptions.WithReadOnly;
+
+            switch (exportItem)
+            {
+                case PlcBlock item:
+                    {
+                        if (item.ProgrammingLanguage == ProgrammingLanguage.ProDiag ||
+                            item.ProgrammingLanguage == ProgrammingLanguage.ProDiag_OB)
+                            return null;
+                        if (item.IsConsistent)
+                        {
+                            // filePath = Path.Combine(filePath, AdjustNames.AdjustFileName(GetObjectName(item)) + ".xml");
+                            if (File.Exists(exportPath))
+                            {
+                                File.Delete(exportPath);
+                            }
+
+                            item.Export(new FileInfo(exportPath), exportOption);
+
+                            return exportPath;
+                        }
+
+                        throw new EngineeringException(string.Format(CultureInfo.InvariantCulture,
+                            "程序块: {0} 是不一致的! 请编译程序块! 导出将终止!", item.Name));
+                    }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// 字符串处理将"/"替换为"_"
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private string StringHandle(string input)
+        {
+            //查询名称是否包含“/”，如果包含替换更“_”
+            string ret = input;
+            while (ret.Contains("/"))
+            {
+                ret = ret.Replace("/", "_");
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// 删除文件夹及其内容
+        /// </summary>
+        /// <param name="targetDir"></param>
+        private static void DeleteDirectoryAndContents(string targetDir)
+        {
+            if (!Directory.Exists(targetDir))
+            {
+                throw new DirectoryNotFoundException($"目录 {targetDir} 不存在。");
+            }
+
+            string[] files = Directory.GetFiles(targetDir);
+            string[] dirs = Directory.GetDirectories(targetDir);
+
+            // 先删除所有文件
+            foreach (string file in files)
+            {
+                File.Delete(file);
+            }
+
+            // 然后递归删除所有子目录
+            foreach (string dir in dirs)
+            {
+                DeleteDirectoryAndContents(dir);
+            }
+
+            // 最后，删除目录本身
+            Directory.Delete(targetDir, false);
+        }
+
+        /// <summary>
+        /// 获取ProjectBase：支持多用户项目
+        /// </summary>
+        private void GetProjectData()
+        {
+            try
+            {
+                // Multi-user support
+                // If TIA Portal is in multiuser environment (connected to project server)
+                if (_tiaPortal.LocalSessions.Any())
+                {
+                    _projectBase = _tiaPortal.LocalSessions
+                        .FirstOrDefault(s => s.Project != null && s.Project.IsPrimary)?.Project;
+                }
+                else
+                {
+                    // Get local project
+                    _projectBase = _tiaPortal.Projects.FirstOrDefault(p => p.IsPrimary);
+                }
+
+                // _projectPath = _projectBase?.Path.FullName;
+                _projectDir = _projectBase?.Path.Directory?.FullName;
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "异常", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// PLC是否为离线模式
+        /// </summary>
+        /// <returns></returns>
+        private bool IsOffline()
+        {
+            bool ret = false;
+
+            foreach (Device device in _projectBase.Devices)
+            {
+                DeviceItem deviceItem = device.DeviceItems[1];
+                if (deviceItem.GetAttribute("Classification") is DeviceItemClassifications.CPU)
+                {
+                    OnlineProvider onlineProvider = deviceItem.GetService<OnlineProvider>();
+                    ret = (onlineProvider.State == OnlineState.Offline);
+                }
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// 获取选中项类型关闭显示项目树按钮
+        /// </summary>
+        /// <param name="menuSelectionProvider"></param>
+        /// <returns></returns>
+        private static MenuStatus GenerateStatus(MenuSelectionProvider<IEngineeringObject> menuSelectionProvider)
+        {
+            var show = false;
+
+            foreach (IEngineeringObject engineeringObject in menuSelectionProvider.GetSelection())
+            {
+                if (!(engineeringObject is GlobalDB))
+                {
+                    show = true;
+                    break;
+                }
+            }
+            return show ? MenuStatus.Disabled : MenuStatus.Hidden;
+        }
+
+        /// <summary>
+        /// AddIn Tester 插件调试器
+        /// </summary>
+        /// <param name="label"></param>
+        /// <returns></returns>
+        public IEnumerable<IEngineeringObject> GetSelection(string label)
+        {
+            PlcSoftware plcSoftware = null;
+            ProjectBase projectBase;
+            
+            if (_tiaPortal.LocalSessions.Any())
+            {
+                projectBase = _tiaPortal.LocalSessions
+                    .FirstOrDefault(s => s.Project != null && s.Project.IsPrimary)?.Project;
+            }
+            else
+            {
+                // Get local project
+                projectBase = _tiaPortal.Projects.FirstOrDefault(p => p.IsPrimary);
+            }
+
+            if (projectBase != null)
+            {
+                foreach (Device device in projectBase.Devices)
+                {
+                    if (device.DeviceItems[1].GetAttribute("Classification") is DeviceItemClassifications.CPU)
+                    {
+                        DeviceItemComposition deviceItemComposition = device.DeviceItems;
+                        foreach (DeviceItem deviceItem in deviceItemComposition)
+                        {
+                            SoftwareContainer softwareContainer = deviceItem.GetService<SoftwareContainer>();
+                            if (softwareContainer != null)
+                            {
+                                Software softwareBase = softwareContainer.Software;
+                                plcSoftware = softwareBase as PlcSoftware;
+                            }
+                        }
+                    }
+                }
+            }
+
+            var selection = new List<IEngineeringObject>();
+
+            if (plcSoftware != null)
+            {
+                selection.Add(plcSoftware.BlockGroup.Blocks.Find("数据块_1"));
+            }
+
+            return selection;
+        }
+    }
+}
---
+++ XmlReader.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace TIA_Add_In_Intf2WCS
+{
+    public class XmlReader
+    {
+        /// <summary>
+        /// 获取Xml的命名空间
+        /// </summary>
+        private XmlNamespaceManager _xmlns;
+
+        /// <summary>
+        /// 获取实例的DB名称
+        /// </summary>
+        public string InstanceName;
+
+        // /// <summary>
+        // /// 获取实例的FB名称
+        // /// </summary>
+        // public string InstanceOfName;
+
+        /// <summary>
+        /// 获取DB的编号
+        /// </summary>
+        public string Number;
+
+        /// <summary>
+        /// 获取程序语言类型
+        /// </summary>
+        public string ProgrammingLanguage;
+
+        /// <summary>
+        /// 获取InstanceDB Xml文件路径
+        /// </summary>
+        public string DbXmlFilePath;
+
+        // /// <summary>
+        // /// 获取FB Xml文件路径
+        // /// </summary>
+        // public string FBXmlFilePath;
+
+        /// <summary>
+        /// 写入数据流
+        /// </summary>
+        public StreamWriter StreamWriter;
+
+        public void Run()
+        {
+            //读取Xml文件
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(DbXmlFilePath);
+            //获取"Sections"节点
+            XmlNode sections = xmlDocument.GetElementsByTagName("Sections")[0];
+            //从Sections和BlockInstSupervisionGroups节点获取NameSpace
+            _xmlns = GetXmlns(xmlDocument, sections.NamespaceURI);
+
+            //获取变量和偏移量
+            foreach (XmlNode members in sections)
+            {
+                foreach (XmlNode member in members)
+                {
+                    var iStruct = GetAttribute(member, "Name");
+                    string iInterface;
+                    string type;
+                    string offset;
+                    string length;
+                    switch (GetAttribute(member, "Datatype"))
+                    {
+                        case "Struct":
+                            foreach (XmlNode item in GetMember(member))
+                            {
+                                iInterface = GetAttribute(item, "Name");
+                                type = GetAttribute(item, "Datatype");
+                                offset = CalOffset(GetOffset(member) + GetOffset(item));
+                                length = GetLength(type);
+                                SetSacdaTag(iStruct, iInterface, type, offset, length);
+                            }
+                            break;
+                        case "\"Conv_To_WCS\"":
+                        case "\"WCS_To_Conv\"":
+                            foreach (XmlNode item in GetSection(member))
+                            {
+                                iInterface = GetAttribute(item, "Name");
+                                type = GetAttribute(item, "Datatype");
+                                offset = CalOffset(GetOffset(member) + GetOffset(item));
+                                length = GetLength(type);
+                                SetSacdaTag(iStruct, iInterface, type, offset, length);
+                            }
+                            break;
+                        default:
+                                iInterface = GetAttribute(member, "Name");
+                                type = GetAttribute(member, "Datatype");
+                                offset = CalOffset(GetOffset(member));
+                                length = GetLength(type);
+                                SetSacdaTag(iStruct, iInterface, type, offset, length);
+                            break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取命名空间
+        /// </summary>
+        /// <param name="xmlDocument"></param>
+        /// <param name="namespaceUri">Sections命名空间</param>
+        /// <returns>xmlns="http://www.siemens.com/automation/Openness/SW/Interface/v5"</returns>
+        private XmlNamespaceManager GetXmlns(XmlDocument xmlDocument, string namespaceUri)
+        {
+            XmlNamespaceManager xmlns = new XmlNamespaceManager(xmlDocument.NameTable);
+            xmlns.AddNamespace("x", namespaceUri);
+            return xmlns;
+        }
+
+        /// <summary>
+        /// 获取节点的元素的值
+        /// </summary>
+        /// <param name="xmlNode"></param>
+        /// <param name="nameItem"></param>
+        /// <returns>Name="MsgType" 或 Datatype="Int"</returns>
+        private static string GetAttribute(XmlNode xmlNode, string nameItem)
+        {
+            var name = xmlNode.Attributes?.GetNamedItem(nameItem).Value;
+            return name;
+        }
+
+        /// <summary>
+        /// 获取Section
+        /// </summary>
+        /// <param name="xmlNode"></param>
+        /// <returns></returns>
+        private XmlNode GetSection(XmlNode xmlNode)
+        {
+            XmlNode section = xmlNode.SelectSingleNode("./x:Sections", _xmlns)?.SelectSingleNode("./x:Section", _xmlns);
+            return section;
+        }
+
+        /// <summary>
+        /// 获取Member
+        /// </summary>
+        /// <param name="xmlNode"></param>
+        /// <returns></returns>
+        private XmlNodeList GetMember(XmlNode xmlNode)
+        {
+            XmlNodeList section = xmlNode.SelectNodes("./x:Member", _xmlns);
+            return section;
+        }
+
+        /// <summary>
+        /// 计算偏移量
+        /// </summary>
+        /// <param name="getOffset"></param>
+        /// <returns>40.1</returns>
+        private static string CalOffset(int getOffset)
+        {
+            //计算偏移量
+            var consult = (getOffset / 8).ToString(); //商，"40"
+            var rem = (getOffset % 8).ToString(); //余数，"1"
+            var offset = $"{consult}.{rem}"; //偏移量结果：40.1
+            // Console.WriteLine(offset);
+
+            return offset;
+        }
+
+        /// <summary>
+        /// 获取偏移量节点
+        /// </summary>
+        /// <param name="xmlNode"></param>
+        /// <returns>321</returns>
+        private int GetOffset(XmlNode xmlNode)
+        {
+            XmlNode integerAttribute = xmlNode.SelectSingleNode("./x:AttributeList", _xmlns)?.FirstChild.FirstChild;
+            if (integerAttribute == null) return 0;
+            var offset = Convert.ToInt32(integerAttribute.Value);
+            return offset;
+        }
+
+        /// <summary>
+        /// 设置数据类型长度
+        /// </summary>
+        /// <param name="dataType"></param>
+        /// <returns></returns>
+        private static string GetLength(string dataType)
+        {
+            string ret;
+
+            switch (dataType)
+            {
+                case "Bool":
+                    ret = "0.1";
+                    break;
+                case "Byte":
+                case "SInt":
+                case "UInt":
+                    ret = "1";
+                    break;
+                case "Int":
+                case "Uint":
+                case "Word":
+                    ret = "2";
+                    break;
+                case "Array[1..20] of Char":
+                    ret = "20";
+                    break;
+                case "DInt":
+                case "DWord":
+                    ret = "4";
+                    break;
+                case "LWord":
+                    ret = "8";
+                    break;
+                case "String":
+                    ret = "254";
+                    break;
+                default:
+                    ret = "0";
+                    break;
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// 设置WCS接口值
+        /// </summary>
+        /// <param name="iStruct">输送机编号</param>
+        /// <param name="iInterface">接口名称</param>
+        /// <param name="datatype">数据类型</param>
+        /// <param name="offset">偏移量</param>
+        /// <param name="length">长度</param>
+        /// <returns>"P1001","MsgType","Int","DB83","0.0","PLC_To_WCS","2"</returns>
+        private void SetSacdaTag(string iStruct, string iInterface, string datatype, string offset, string length)
+        {
+            var str =
+                $"\"{iStruct}\"," +
+                $"\"{iInterface}\"," +
+                $"\"{datatype}\"," +
+                $"\"{ProgrammingLanguage}{Number}\"," +
+                $"\"{offset}\"," +
+                $"\"{InstanceName}\",\"{length}\"";
+            StreamWriter.WriteLine(str);
+            Console.WriteLine(str);
+        }
+    }
+}
